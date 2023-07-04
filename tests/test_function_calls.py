bc3aa126f1ca6e8cf2dbc090aac7dc374de069ff
DaveOkpare <okparedave@gmail.com>
2023-07-04 19:48:46 +0100

    Added test_function_calls.py

---
+++ tests/test_function_calls.py
@@ -0,0 +1,31 @@
+import pytest
+from pydantic import BaseModel
+
+from openai_function_call import OpenAISchema, openai_schema
+
+
+def test_openai_schema():
+    @openai_schema
+    class Dataframe(BaseModel):
+        """
+        Class representing a dataframe. This class is used to convert
+        data into a frame that can be used by pandas.
+        """
+        data: str
+        columns: str
+
+        def to_pandas(self):
+            pass
+
+    assert issubclass(Dataframe, OpenAISchema)
+
+    assert hasattr(Dataframe, "openai_schema")
+    assert hasattr(Dataframe, "from_response")
+    assert hasattr(Dataframe, "to_pandas")
+
+
+def test_openai_schema_raises_error():
+    with pytest.raises(TypeError, match="must be a subclass of pydantic.BaseModel"):
+        @openai_schema
+        class Dummy:
+            pass
