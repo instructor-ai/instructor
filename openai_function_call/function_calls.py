6c1118d3dc70ee5b10d20399c8c609093a0e2047
David Okpare <okparedave@gmail.com>
2023-07-05 17:37:37 +0100

    Added openai_schema decorator (#34)

--- openai_function_call/function_calls.py
+++ openai_function_call/function_calls.py
@@ -111,42 +111,12 @@ class OpenAISchema(BaseModel):
         return cls(**arguments)
 
 
-def MultiTask(
-    subtask_class: Type[OpenAISchema],
-    name: Optional[str] = None,
-    description: Optional[str] = None,
-):
-    """
-    Dynamically create a MultiTask OpenAISchema that can be used to segment multiple
-    tasks given a base class. This creates class that can be used to create a toolkit
-    for a specific task, names and descriptions are automatically generated. However
-    they can be overridden.
+def openai_schema(cls):
+    if not issubclass(cls, BaseModel):
+        raise TypeError("Class must be a subclass of pydantic.BaseModel")
 
-    :param subtask_class: The base class to use for the MultiTask
-    :param name: The name of the MultiTask
-    :param description: The description of the MultiTask
+    @wraps(cls, updated=())
+    class Wrapper(cls, OpenAISchema):  # type: ignore
+        pass
 
-    :return: new schema class called `Multi{subtask_class.name}`
-    """
-    task_name = subtask_class.__name__ if name is None else name
-
-    name = f"Multi{task_name}"
-
-    list_tasks = (
-        List[subtask_class],
-        Field(
-            default_factory=list,
-            repr=False,
-            description=f"Correctly segmented list of `{task_name}` tasks",
-        ),
-    )
-
-    new_cls = create_model(name, tasks=list_tasks, __base__=(OpenAISchema,))
-
-    new_cls.__doc__ = (
-        f"Correct segmentation of `{task_name}` tasks"
-        if description is None
-        else description
-    )
-
-    return new_cls
+    return Wrapper
