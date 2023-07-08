9b95ba805d5de5dc4671acadfdfda6c293e292b1
Jason Liu <jxnl@users.noreply.github.com>
2023-07-08 21:33:07 +0800

    Docs! Help wanted, adding examples. (#44)
    * add docs
    
    * bump

--- openai_function_call/function_calls.py
+++ openai_function_call/function_calls.py
@@ -84,6 +84,9 @@ class OpenAISchema(BaseModel):
     @classmethod
     @property
     def openai_schema(cls):
+        """
+        Return the schema of the class in the format of OpenAI's schema
+        """
         schema = cls.schema()
         parameters = {
             k: v for k, v in schema.items() if k not in ("title", "description")
@@ -98,6 +101,7 @@ class OpenAISchema(BaseModel):
 
     @classmethod
     def from_response(cls, completion, throw_error=True):
+        """Execute the function from the response of an openai chat completion"""
         message = completion.choices[0].message
 
         if throw_error:
