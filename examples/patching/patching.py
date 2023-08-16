944366847f4e13354fad94e0842e66b08d2317d0
Jason Liu <jxnl@users.noreply.github.com>
2023-08-17 01:25:29 +0800

    Support patching openai (#78)
    * update docs
    
    * add patch
    
    * bump version

---
+++ examples/patching/patching.py
@@ -0,0 +1,23 @@
+import openai
+from pydantic import BaseModel
+from openai_function_call import patch
+
+# By default, the patch function will patch the ChatCompletion.create and ChatCompletion.acreate methods. to support response_model parameter
+patch()
+
+
+# Now, we can use the response_model parameter using only a base model
+# rather than having to use the OpenAISchema class
+class UserExtract(BaseModel):
+    name: str
+    age: int
+
+user: UserExtract = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    response_model=UserExtract,
+    messages=[
+        {"role": "user", "content": "Extract jason is 25 years old"},
+    ],
+)  # type: ignore
+
+print(user)
--- openai_function_call/__init__.py
+++ openai_function_call/__init__.py
@@ -1,4 +1,11 @@
 from .function_calls import OpenAISchema, openai_function, openai_schema
 from .dsl.multitask import MultiTask
+from .patch import patch
 
-__all__ = ["OpenAISchema", "openai_function", "MultiTask", "openai_schema"]
+__all__ = [
+    "OpenAISchema",
+    "openai_function",
+    "MultiTask",
+    "openai_schema",
+    "patch",
+]
