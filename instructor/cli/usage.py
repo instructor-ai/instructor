93330c4d742361457318af12cea955518ab2c450
Jason <jason@jxnl.co>
2023-09-05 21:11:52 -0500

    updat

--- instructor/cli/usage.py
+++ instructor/cli/usage.py
@@ -23,34 +23,18 @@ async def fetch_usage(date: str) -> dict:
             return await resp.json()
 
 
-def generate_usage_table(usage_data: List[dict]) -> Table:
-    table = Table(title="OpenAI API Usage")
-    table.add_column("Timestamp", style="dim")
-    table.add_column("Requests", justify="right")
-    table.add_column("Snapshot ID")
-    table.add_column("Context Tokens")
-    table.add_column("Generated Tokens")
-
-    for usage in usage_data:
-        table.add_row(
-            str(datetime.fromtimestamp(usage["aggregation_timestamp"])),
-            str(usage["n_requests"]),
-            usage["snapshot_id"],
-            str(usage["n_context_tokens_total"]),
-            str(usage["n_generated_tokens_total"]),
-        )
-    return table
-
-
 async def get_usage_for_past_n_days(n_days: int) -> List[dict]:
     tasks = []
     all_data = []
     with Progress() as progress:
-        task = progress.add_task("[green]Fetching usage data...", total=n_days)
-        for i in range(n_days):
-            date = (datetime.now() - timedelta(days=i)).strftime("%Y-%m-%d")
-            tasks.append(fetch_usage(date))
-            progress.update(task, advance=1)
+        if n_days > 1:
+            task = progress.add_task("[green]Fetching usage data...", total=n_days)
+            for i in range(n_days):
+                date = (datetime.now() - timedelta(days=i)).strftime("%Y-%m-%d")
+                tasks.append(fetch_usage(date))
+                progress.update(task, advance=1)
+        else:
+            tasks.append(fetch_usage(datetime.now().strftime("%Y-%m-%d")))
 
         fetched_data = await asyncio.gather(*tasks)
         for data in fetched_data:
@@ -58,9 +42,60 @@ async def get_usage_for_past_n_days(n_days: int) -> List[dict]:
     return all_data
 
 
+from collections import defaultdict
+from datetime import datetime
+from typing import List
+from rich.table import Table
+
+# Define the cost per unit for each model
+MODEL_COSTS = {
+    "gpt-3.5-turbo": {"prompt": 0.0015 / 1000, "completion": 0.002 / 1000},
+    "gpt-3.5-turbo-16k": {"prompt": 0.003 / 1000, "completion": 0.004 / 1000},
+    "gpt-4": {"prompt": 0.03 / 1000, "completion": 0.03 / 1000},
+    "gpt-4-32k": {"prompt": 0.06 / 1000, "completion": 0.12 / 1000},
+    "text-embedding-ada-002": 0.0001 / 1000,
+    "text-embedding-ada-002-v2": 0.0001 / 1000,
+}
+
+
+def get_model_cost(model: str):
+    """Get the cost details for a given model."""
+    if model in MODEL_COSTS:
+        return MODEL_COSTS[model]
+
+    # Handle prefix-based matching
+    if model.startswith("gpt-3.5-turbo-16k"):
+        return MODEL_COSTS["gpt-3.5-turbo-16k"]
+    elif model.startswith("gpt-3.5-turbo"):
+        return MODEL_COSTS["gpt-3.5-turbo"]
+    elif model.startswith("gpt-4-32k"):
+        return MODEL_COSTS["gpt-4-32k"]
+    elif model.startswith("gpt-4"):
+        return MODEL_COSTS["gpt-4"]
+    else:
+        raise ValueError(f"Cost for model {model} not found")
+
+
+def calculate_cost(
+    snapshot_id: str, n_context_tokens: int, n_generated_tokens: int
+) -> float:
+    """Calculate the cost based on the snapshot ID and number of tokens."""
+    cost = get_model_cost(snapshot_id)
+
+    if isinstance(cost, float):
+        return cost * (n_context_tokens + n_generated_tokens)
+
+    prompt_cost = cost["prompt"] * n_context_tokens
+    completion_cost = cost["completion"] * n_generated_tokens
+    return prompt_cost + completion_cost
+
+
 def group_and_sum_by_date_and_snapshot(usage_data: List[dict]) -> Table:
+    """Group and sum the usage data by date and snapshot, including costs."""
     summary = defaultdict(
-        lambda: defaultdict(lambda: {"total_requests": 0, "total_tokens": 0})
+        lambda: defaultdict(
+            lambda: {"total_requests": 0, "total_tokens": 0, "total_cost": 0.0}
+        )
     )
 
     for usage in usage_data:
@@ -71,11 +106,19 @@ def group_and_sum_by_date_and_snapshot(usage_data: List[dict]) -> Table:
         summary[date][snapshot_id]["total_requests"] += usage["n_requests"]
         summary[date][snapshot_id]["total_tokens"] += usage["n_generated_tokens_total"]
 
-    table = Table(title="Usage Summary by Date and Snapshot")
+        # Calculate and add the cost
+        cost = calculate_cost(
+            snapshot_id,
+            usage["n_context_tokens_total"],
+            usage["n_generated_tokens_total"],
+        )
+        summary[date][snapshot_id]["total_cost"] += cost
+
+    table = Table(title="Usage Summary by Date, Snapshot, and Cost")
     table.add_column("Date", style="dim")
-    table.add_column("Snapshot ID", style="dim")
+    table.add_column("Model", style="dim")
     table.add_column("Total Requests", justify="right")
-    table.add_column("Total Tokens", justify="right")
+    table.add_column("Total Cost ($)", justify="right")
 
     for date, snapshots in summary.items():
         for snapshot_id, data in snapshots.items():
@@ -83,7 +126,7 @@ def group_and_sum_by_date_and_snapshot(usage_data: List[dict]) -> Table:
                 date,
                 snapshot_id,
                 str(data["total_requests"]),
-                str(data["total_tokens"]),
+                "{:.2f}".format(data["total_cost"]),
             )
 
     return table
@@ -91,19 +134,10 @@ def group_and_sum_by_date_and_snapshot(usage_data: List[dict]) -> Table:
 
 @app.command(help="Displays OpenAI API usage data for the past N days.")
 def list(
-    n: int = typer.Option(5, help="Number of days."),
+    n: int = typer.Option(0, help="Number of days."),
 ):
     all_data = asyncio.run(get_usage_for_past_n_days(n))
     table = group_and_sum_by_date_and_snapshot(all_data)
-    console.print(table)
-
-
-@app.command(
-    help="Groups the OpenAI API usage data by snapshot_id and sums the total tokens."
-)
-def usage_group_by_snapshot():
-    usage_data = asyncio.run(fetch_usage(datetime.now().strftime("%Y-%m-%d")))
-    table = group_and_sum_by_snapshot(usage_data.get("data", []))
     console.print(table)
 
 
