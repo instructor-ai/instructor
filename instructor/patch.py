1cc45e3faf5d59d4936df01616ba6876ed78a93c
Jason Liu <jxnl@users.noreply.github.com>
2023-09-08 00:58:29 -0500

    Reasking logic on validations  (#98)
    * working cleaned up patch
    
    * Reasking logic
    
    * clean up
    
    * remove
    
    * clean up tests

--- instructor/patch.py
+++ instructor/patch.py
@@ -1,72 +1,14 @@
 from functools import wraps
+from json import JSONDecodeError
+from pydantic import ValidationError
 import openai
 import inspect
-from typing import Callable, Optional, Type, Union
+from typing import Callable, Type
 
 from pydantic import BaseModel
 from .function_calls import OpenAISchema, openai_schema
 
-
-def wrap_chatcompletion(func: Callable) -> Callable:
-    is_async = inspect.iscoroutinefunction(func)
-    if is_async:
-
-        @wraps(func)
-        async def new_chatcompletion(
-            *args,
-            response_model: Optional[Union[Type[BaseModel], Type[OpenAISchema]]] = None,
-            **kwargs
-        ):  # type: ignore
-            if response_model is not None:
-                if not issubclass(response_model, OpenAISchema):
-                    response_model = openai_schema(response_model)
-                kwargs["functions"] = [response_model.openai_schema]
-                kwargs["function_call"] = {"name": response_model.openai_schema["name"]}
-
-            if kwargs.get("stream", False) and response_model is not None:
-                import warnings
-
-                warnings.warn(
-                    "stream=True is not supported when using response_model parameter"
-                )
-
-            response = await func(*args, **kwargs)
-
-            if response_model is not None:
-                model = response_model.from_response(response)
-                model._raw_response = response
-                return model
-            return response
-
-    else:
-
-        @wraps(func)
-        def new_chatcompletion(
-            *args,
-            response_model: Optional[Union[Type[BaseModel], Type[OpenAISchema]]] = None,
-            **kwargs
-        ):
-            if response_model is not None:
-                if not issubclass(response_model, OpenAISchema):
-                    response_model = openai_schema(response_model)
-                kwargs["functions"] = [response_model.openai_schema]
-                kwargs["function_call"] = {"name": response_model.openai_schema["name"]}
-
-            if kwargs.get("stream", False) and response_model is not None:
-                import warnings
-
-                warnings.warn(
-                    "stream=True is not supported when using response_model parameter"
-                )
-
-            response = func(*args, **kwargs)
-            if response_model is not None:
-                model = response_model.from_response(response)
-                model._raw_response = response
-                return model
-            return response
-
-    new_chatcompletion.__doc__ = """
+OVERRIDE_DOCS = """
 Creates a new chat completion for the provided messages and parameters.
 
 See: https://platform.openai.com/docs/api-reference/chat-completions/create
@@ -82,8 +24,118 @@ If need to obtain the raw response from OpenAI's API, you can access it using th
 Parameters:
 
     response_model (Union[Type[BaseModel], Type[OpenAISchema]]): The response model to use for parsing the response from OpenAI's API, if available (default: None)
+    max_retries (int): The maximum number of retries to attempt if the response is not valid (default: 0)
 """
-    return new_chatcompletion
+
+
+def handle_response_model(response_model: Type[BaseModel], kwargs):
+    new_kwargs = kwargs.copy()
+    if response_model is not None:
+        if not issubclass(response_model, OpenAISchema):
+            response_model = openai_schema(response_model)  # type: ignore
+        new_kwargs["functions"] = [response_model.openai_schema]  # type: ignore
+        new_kwargs["function_call"] = {"name": response_model.openai_schema["name"]}  # type: ignore
+
+    if new_kwargs.get("stream", False) and response_model is not None:
+        import warnings
+
+        warnings.warn(
+            "stream=True is not supported when using response_model parameter"
+        )
+
+    return response_model, new_kwargs
+
+
+def process_response(response, response_model):  # type: ignore
+    if response_model is not None:
+        model = response_model.from_response(response)
+        model._raw_response = response
+        return model
+    return response
+
+
+async def retry_async(func, response_model, args, kwargs, max_retries):
+    retries = 0
+    while retries <= max_retries:
+        try:
+            response = await func(*args, **kwargs)
+            return process_response(response, response_model), None
+        except (ValidationError, JSONDecodeError) as e:
+            kwargs["messages"].append(dict(**response.choices[0].message))
+            kwargs["messages"].append(
+                {
+                    "role": "user",
+                    "content": f"Recall the function correctly, exceptions found\n{e}",
+                }
+            )
+            retries += 1
+            if retries > max_retries:
+                raise e
+
+
+def retry_sync(func, response_model, args, kwargs, max_retries):
+    retries = 0
+    new_kwargs = kwargs.copy()
+    while retries <= max_retries:
+        # Excepts ValidationError, and JSONDecodeError
+        try:
+            response = func(*args, **kwargs)
+            return process_response(response, response_model), None
+        except (ValidationError, JSONDecodeError) as e:
+            kwargs["messages"].append(dict(**response.choices[0].message))
+            kwargs["messages"].append(
+                {
+                    "role": "user",
+                    "content": f"Recall the function correctly, exceptions found\n{e}",
+                }
+            )
+            retries += 1
+            if retries > max_retries:
+                raise e
+
+
+def wrap_chatcompletion(func: Callable) -> Callable:
+    is_async = inspect.iscoroutinefunction(func)
+
+    @wraps(func)
+    async def new_chatcompletion_async(response_model, *args, max_retries=0, **kwargs):
+        response_model, new_kwargs = handle_response_model(response_model, kwargs)
+        response, error = await retry_async(
+            func=func,
+            response_model=response_model,
+            max_retries=max_retries,
+            args=args,
+            kwargs=new_kwargs,
+        )  # type: ignore
+        if error:
+            raise ValueError(error)
+        return process_response(response, response_model)
+
+    @wraps(func)
+    def new_chatcompletion_sync(response_model, *args, max_retries=0, **kwargs):
+        response_model, new_kwargs = handle_response_model(response_model, kwargs)
+        response, error = retry_sync(
+            func=func,
+            response_model=response_model,
+            max_retries=max_retries,
+            args=args,
+            kwargs=new_kwargs,
+        )  # type: ignore
+        if error:
+            raise ValueError(error)
+        return response
+
+    wrapper_function = new_chatcompletion_async if is_async else new_chatcompletion_sync
+    wrapper_function.__doc__ = OVERRIDE_DOCS
+    return wrapper_function
+
+
+def process_response(response, response_model):
+    if response_model is not None:
+        model = response_model.from_response(response)
+        model._raw_response = response
+        return model
+    return response
 
 
 original_chatcompletion = openai.ChatCompletion.create
--- tests/test_patch.py
+++ tests/test_patch.py
@@ -4,7 +4,7 @@ import openai
 from instructor import patch
 
 
-@pytest.mark.skip(reason="Needs openai call")
+@pytest.mark.skip("Not implemented")
 def test_runmodel():
     patch()
 
@@ -21,6 +21,38 @@ def test_runmodel():
     )
     assert isinstance(model, UserExtract), "Should be instance of UserExtract"
     assert model.name.lower() == "jason"
+    assert hasattr(
+        model, "_raw_response"
+    ), "The raw response should be available from OpenAI"
+
+
+@pytest.mark.skip("Not implemented")
+def test_runmodel_validator():
+    patch()
+
+    from pydantic import field_validator
+
+    class UserExtract(BaseModel):
+        name: str
+        age: int
+
+        @field_validator("name")
+        @classmethod
+        def validate_name(cls, v):
+            if v.upper() != v:
+                raise ValueError("Name should be uppercase")
+            return v
+
+    model = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        response_model=UserExtract,
+        max_retries=2,
+        messages=[
+            {"role": "user", "content": "Extract jason is 25 years old"},
+        ],
+    )
+    assert isinstance(model, UserExtract), "Should be instance of UserExtract"
+    assert model.name == "JASON"
     assert hasattr(
         model, "_raw_response"
     ), "The raw response should be available from OpenAI"
