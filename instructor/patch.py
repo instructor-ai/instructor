85bf9f10f785b1f11cf0ae84a57b31c1710ad592
Jason <jason@jxnl.co>
2023-09-08 00:40:11 -0400

    working cleaned up patch

--- instructor/patch.py
+++ instructor/patch.py
@@ -6,67 +6,7 @@ from typing import Callable, Optional, Type, Union
 from pydantic import BaseModel
 from .function_calls import OpenAISchema, openai_schema
 
-
-def wrap_chatcompletion(func: Callable) -> Callable:
-    is_async = inspect.iscoroutinefunction(func)
-    if is_async:
-
-        @wraps(func)
-        async def new_chatcompletion(
-            *args,
-            response_model: Optional[Union[Type[BaseModel], Type[OpenAISchema]]] = None,
-            **kwargs
-        ):  # type: ignore
-            if response_model is not None:
-                if not issubclass(response_model, OpenAISchema):
-                    response_model = openai_schema(response_model)
-                kwargs["functions"] = [response_model.openai_schema]
-                kwargs["function_call"] = {"name": response_model.openai_schema["name"]}
-
-            if kwargs.get("stream", False) and response_model is not None:
-                import warnings
-
-                warnings.warn(
-                    "stream=True is not supported when using response_model parameter"
-                )
-
-            response = await func(*args, **kwargs)
-
-            if response_model is not None:
-                model = response_model.from_response(response)
-                model._raw_response = response
-                return model
-            return response
-
-    else:
-
-        @wraps(func)
-        def new_chatcompletion(
-            *args,
-            response_model: Optional[Union[Type[BaseModel], Type[OpenAISchema]]] = None,
-            **kwargs
-        ):
-            if response_model is not None:
-                if not issubclass(response_model, OpenAISchema):
-                    response_model = openai_schema(response_model)
-                kwargs["functions"] = [response_model.openai_schema]
-                kwargs["function_call"] = {"name": response_model.openai_schema["name"]}
-
-            if kwargs.get("stream", False) and response_model is not None:
-                import warnings
-
-                warnings.warn(
-                    "stream=True is not supported when using response_model parameter"
-                )
-
-            response = func(*args, **kwargs)
-            if response_model is not None:
-                model = response_model.from_response(response)
-                model._raw_response = response
-                return model
-            return response
-
-    new_chatcompletion.__doc__ = """
+OVERRIDE_DOCS = """
 Creates a new chat completion for the provided messages and parameters.
 
 See: https://platform.openai.com/docs/api-reference/chat-completions/create
@@ -83,7 +23,67 @@ Parameters:
 
     response_model (Union[Type[BaseModel], Type[OpenAISchema]]): The response model to use for parsing the response from OpenAI's API, if available (default: None)
 """
-    return new_chatcompletion
+
+
+def handle_response_model(response_model: Type[BaseModel], **kwargs):
+    new_kwargs = kwargs.copy()
+    if response_model is not None:
+        if not issubclass(response_model, OpenAISchema):
+            response_model = openai_schema(response_model)  # type: ignore
+        new_kwargs["functions"] = [response_model.openai_schema]  # type: ignore
+        new_kwargs["function_call"] = {"name": response_model.openai_schema["name"]}  # type: ignore
+
+    if new_kwargs.get("stream", False) and response_model is not None:
+        import warnings
+
+        warnings.warn(
+            "stream=True is not supported when using response_model parameter"
+        )
+
+    return response_model, new_kwargs
+
+
+def process_response(response, response_model):  # type: ignore
+    if response_model is not None:
+        model = response_model.from_response(response)
+        model._raw_response = response
+        return model
+    return response
+
+
+def wrap_chatcompletion(func: Callable) -> Callable:
+    is_async = inspect.iscoroutinefunction(func)
+
+    @wraps(func)
+    async def new_chatcompletion_async(
+        response_model=Type[BaseModel], retry=None, *args, **kwargs
+    ):
+        response_model, new_kwargs = handle_response_model(
+            response_model=response_model, **kwargs
+        )
+
+        response = await func(*args, **new_kwargs)
+        return process_response(response, response_model)
+
+    @wraps(func)
+    def new_chatcompletion_sync(response_model=Type[BaseModel], *args, **kwargs):
+        response_model, new_kwargs = handle_response_model(
+            response_model=response_model, **kwargs
+        )
+        response = func(*args, **new_kwargs)
+        return process_response(response, response_model)
+
+    wrapper_function = new_chatcompletion_async if is_async else new_chatcompletion_sync
+    wrapper_function.__doc__ = OVERRIDE_DOCS
+    return wrapper_function
+
+
+def process_response(response, response_model):
+    if response_model is not None:
+        model = response_model.from_response(response)
+        model._raw_response = response
+        return model
+    return response
 
 
 original_chatcompletion = openai.ChatCompletion.create
