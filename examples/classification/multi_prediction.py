3f0e0477a92ac306ca3e9b2c6ee703dbae16da15
Jason <jason@jxnl.co>
2023-09-13 22:52:09 -0400

    fix multi commit

--- examples/classification/multi_prediction.py
+++ examples/classification/multi_prediction.py
@@ -9,18 +9,14 @@ patch()
 
 # Define new Enum class for multiple labels
 class MultiLabels(str, enum.Enum):
-    TECH_ISSUE = "tech_issue"
     BILLING = "billing"
     GENERAL_QUERY = "general_query"
+    HARDWARE = "hardware"
 
 
 # Adjust the prediction model to accommodate a list of labels
 class MultiClassPrediction(BaseModel):
-    """
-    List of correct class labels for the given text (Multi Class)
-    """
-
-    class_labels: List[MultiLabels]
+    predicted_labels: List[MultiLabels]
 
 
 # Modify the classify function
@@ -38,7 +34,8 @@ def multi_classify(data: str) -> MultiClassPrediction:
 
 
 # Example using a support ticket
-ticket = "My account is locked and I can't access my billing info."
+ticket = (
+    "My account is locked and I can't access my billing info. Phone is also broken."
+)
 prediction = multi_classify(ticket)
-assert MultiLabels.TECH_ISSUE in prediction.class_labels
-assert MultiLabels.BILLING in prediction.class_labels
+print(prediction)
