2649061ff157fc96f21a5be5468d2ec2f13dc89b
Ankur Goyal <ankrgyl@gmail.com>
2023-11-26 15:21:47 -0800

    Add braintrust proxy (#225)

--- tests/openai/evals/conftest.py
+++ tests/openai/evals/conftest.py
@@ -1,13 +1,39 @@
 # conftest.py
 from openai import AsyncOpenAI, OpenAI
 import pytest
+import os
+
+try:
+    import braintrust
+
+    wrap_openai = braintrust.wrap_openai
+except ImportError:
+
+    def wrap_openai(x):
+        return x
 
 
 @pytest.fixture(scope="session")
 def client():
-    yield OpenAI()
+    if os.environ.get("BRAINTRUST_API_KEY"):
+        yield wrap_openai(
+            OpenAI(
+                api_key=os.environ["BRAINTRUST_API_KEY"],
+                base_url="https://braintrustproxy.com/v1",
+            )
+        )
+    else:
+        yield OpenAI()
 
 
 @pytest.fixture(scope="session")
 def aclient():
-    yield AsyncOpenAI()
+    if os.environ.get("BRAINTRUST_API_KEY"):
+        yield wrap_openai(
+            AsyncOpenAI(
+                api_key=os.environ["BRAINTRUST_API_KEY"],
+                base_url="https://braintrustproxy.com/v1",
+            )
+        )
+    else:
+        yield AsyncOpenAI()
