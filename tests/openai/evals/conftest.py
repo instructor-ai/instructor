cdfbb0315a32432094bd49d58c2911d7029d9373
Ankur Goyal <ankrgyl@gmail.com>
2023-11-26 11:16:49 -0800

    Use proxy if BRAINTRUST_API_KEY set

--- tests/openai/evals/conftest.py
+++ tests/openai/evals/conftest.py
@@ -1,13 +1,41 @@
 # conftest.py
 from openai import AsyncOpenAI, OpenAI
 import pytest
+import os
+
+try:
+    import braintrust
+
+    wrap_openai = braintrust.wrap_openai
+except ImportError:
+
+    def wrap_openai(x):
+        return x
+
+    pass
 
 
 @pytest.fixture(scope="session")
 def client():
-    yield OpenAI()
+    if "BRAINTRUST_API_KEY" in os.environ:
+        yield wrap_openai(
+            OpenAI(
+                api_key=os.environ["BRAINTRUST_API_KEY"],
+                base_url="https://proxy.braintrustapi.com/v1",
+            )
+        )
+    else:
+        yield OpenAI()
 
 
 @pytest.fixture(scope="session")
 def aclient():
-    yield AsyncOpenAI()
+    if "BRAINTRUST_API_KEY" in os.environ:
+        yield wrap_openai(
+            AsyncOpenAI(
+                api_key=os.environ["BRAINTRUST_API_KEY"],
+                base_url="https://proxy.braintrustapi.com/v1",
+            )
+        )
+    else:
+        yield AsyncOpenAI()
