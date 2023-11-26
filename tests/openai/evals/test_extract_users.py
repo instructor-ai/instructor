8f88fef9ac2e01c09ec8cee8cf36e9640fc4d9d2
Ankur Goyal <ankrgyl@gmail.com>
2023-11-26 10:44:07 -0800

    Add braintrust logging

--- tests/openai/evals/test_extract_users.py
+++ tests/openai/evals/test_extract_users.py
@@ -1,3 +1,5 @@
+import braintrust
+import uuid
 import pytest
 from itertools import product
 from pydantic import BaseModel
@@ -21,6 +23,8 @@ test_data = [
 ]
 modes = [Mode.FUNCTIONS, Mode.JSON, Mode.TOOLS]
 
+run_id = uuid.uuid4().hex
+
 
 @pytest.mark.parametrize("model, data, mode", product(models, test_data, modes))
 def test_extract(model, data, mode):
@@ -35,28 +39,48 @@ def test_extract(model, data, mode):
             "FUNCTIONS mode is not supported for meta/llama-2-70b-chat, skipping test"
         )
 
-    # Setting up the client with the instructor patch
-    client = instructor.patch(
-        OpenAI(
-            base_url="https://proxy.braintrustapi.com/v1",
-            api_key=os.environ["BRAINTRUST_API_KEY"],
-        ),
-        mode=mode,
-    )
+    with braintrust.init(
+        project="instructor_text_extract",
+        experiment=f"{model}_{mode}_{run_id[:8]}",
+        update=True,
+        org_name="braintrustdata.com",
+    ) as experiment:
+        with experiment.start_span(
+            input=sample_data, expected={"name": expected_name, "age": expected_age}
+        ) as span:
+            # Setting up the client with the instructor patch
+            client = instructor.patch(
+                braintrust.wrap_openai(
+                    OpenAI(
+                        base_url="https://proxy.braintrustapi.com/v1",
+                        api_key=os.environ["BRAINTRUST_API_KEY"],
+                    )
+                ),
+                mode=mode,
+            )
 
-    # Calling the extract function with the provided model, sample data, and mode
-    response = client.chat.completions.create(
-        model=model,
-        response_model=UserDetails,
-        messages=[
-            {"role": "user", "content": sample_data},
-        ],
-    )
+            # Calling the extract function with the provided model, sample data, and mode
+            response = client.chat.completions.create(
+                model=model,
+                response_model=UserDetails,
+                messages=[
+                    {"role": "user", "content": sample_data},
+                ],
+            )
 
-    # Assertions
-    assert (
-        response.name == expected_name
-    ), f"Expected name {expected_name}, got {response.name}"
-    assert (
-        response.age == expected_age
-    ), f"Expected age {expected_age}, got {response.age}"
+            span.log(
+                output=response.dict(),
+                scores={
+                    "name": response.name == expected_name,
+                    "age": response.age == expected_age,
+                },
+            )
+
+        print(experiment.summarize())
+        # Assertions
+        assert (
+            response.name == expected_name
+        ), f"Expected name {expected_name}, got {response.name}"
+        assert (
+            response.age == expected_age
+        ), f"Expected age {expected_age}, got {response.age}"
