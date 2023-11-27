ab672c371588351e3fc4873d052ebe9ef314dd6b
Brandon phillips <thedefinedone@gmail.com>
2023-11-26 19:18:08 -0700

    Refactored code to utilize Maybe to wrap model.

--- examples/open_source_examples/openrouter.py
+++ examples/open_source_examples/openrouter.py
@@ -1,38 +1,97 @@
-import instructor
+import os
 import openai
 from pydantic import BaseModel, Field
 from typing import Optional
-from instructor import Mode
+from instructor import Mode, patch, Maybe
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
 
-# Load in your API keys
-openrouter_api_key = "OPENROUTER_API_KEY"
+# Extract API key from environment
+openrouter_api_key = os.environ.get("OPENROUTER_API_KEY")
+if not openrouter_api_key:
+    raise ValueError("OPENROUTER_API_KEY is not set in environment variables")
 
-# Set your OpenAI base_url to point to Open router base OpenAI API spec endpoint
+# Base URL for OpenAI
 openrouter_base_url = "https://openrouter.ai/api/v1"
 
-# Create Instructor patched OpenAI client, ensure you're utilizing JSON mode.
-client = instructor.patch(openai.OpenAI(api_key=openrouter_api_key, base_url=openrouter_base_url), mode=Mode.JSON)
+# Pydantic model for user details
 
 
-# Create our simple pydantic UserDetail BaseModel
 class UserDetail(BaseModel):
-    name: str = Field(..., description="Name of the user")
-    age: int
-    occupation: Optional[str] = Field(None, description="Occupation of the user")
+    """Details in the text about the user"""
+    name: str = Field(description="Name extracted from the text")
+    age: int = Field(description="Age extracted from the text")
+    occupation: Optional[str] = Field(
+        default=None, description="Occupation extracted from the text")
 
-# Utilizing teknium/openhermes-2.5-mistral-7b, we need to ensure the system prompt is present
+
+def create_openai_client(api_key: str, base_url: str):
+    """
+    Create and patch OpenAI client.
+    """
+    client = openai.OpenAI(api_key=api_key, base_url=base_url)
+    return patch(client, mode=Mode.JSON)
+
+
+def get_user_details(client, model: str, user_message: dict) -> Maybe(UserDetail):
+    """
+    Extract user details using Open soure model.
+    """
+    maybe_user_detail = Maybe(UserDetail)
+    system_message = {
+        "role": "system",
+        "content": f"You are an expert at outputting json. You always output valid pydantic model_json_schemas."
+    }
+    try:
+        response = client.chat.completions.create(
+            response_model=Maybe(UserDetail),
+            response_format={"type": "json_object"},
+            model=model,
+            messages=[system_message, user_message]
+        )
+    except Exception as e:
+        return maybe_user_detail(error=True, result=None, message=str(e))
+    return response
+
+
+def create_user_message(role: str, content: str) -> dict:
+    maybe_detail = Maybe(UserDetail)
+    return {
+        "role": role,
+        "content": f"Extract the user details from the following text: {content}. Match your response to this json_schema:{maybe_detail.model_json_schema()}"
+    }
+
+
+def create_retry_message(role: str, error: str, content: str) -> dict:
+    return [{
+        "role": role,
+        "content": f"You experienced an error with the previous context: {error}. Please try again."
+    }, {
+        "role": "user",
+        "content": f"Extract the user details from the following text: {content}"
+    }]
+
+
+# Initialize OpenAI client
+client = create_openai_client(openrouter_api_key, openrouter_base_url)
+
+# Define model and user message
 model = "teknium/openhermes-2.5-mistral-7b"
-system_message = {"role": "system", "content": f"You are an expert at outputting json. Match your response to this json_schema:{UserDetail.model_json_schema()['properties']}"}
-user_message = {"role": "user","content": "Extract the user details from the following text: Brandon is 33 years old. He works as a solution architect."}
 
-# Let's call the model
-user = client.chat.completions.create(response_format={"type": "json_object"}, model=model, messages=[system_message,user_message])
+# Create items to evaluate output
+user_detail_messages = ["Brandon is 33 years old. He works as a solution architect.",
+                        "Jason is 25 years old. He is the GOAT.", "Dominic is 45 years old. He is retired.", "Jenny is 72. She is a wife and a CEO.", "Holly is 22. She is an explorer.", "There onces was a prince, named Benny. He ruled for 10 years, which just ended. He started at 22.", "Simon says, why are you 22 years old marvin?"]
 
-# Unmarshal Json
-detail = UserDetail.model_validate_json(user.choices[0].message.content)
 
-# Make assertions
-assert isinstance(detail, UserDetail)
-assert detail.name == "Brandon"
-assert detail.age == 33
+for message in user_detail_messages:
+    user_message = create_user_message("user", message)
+    user = get_user_details(client, model, user_message)
+    # Assertions for validation
+    if user.error:
+        raise ValueError(user.error)
+    if user.result:
+        assert isinstance(user.result, UserDetail)
+        print(user.result.name)
+       
