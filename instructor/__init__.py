0aeb41c1cb5a344a45ab64ea42d00cbaad84b9f3
Jason Liu <jason@jxnl.co>
2023-12-09 15:56:34 -0500

    remove function decorator

--- instructor/__init__.py
+++ instructor/__init__.py
@@ -1,11 +1,10 @@
 from .distil import FinetuneFormat, Instructions
 from .dsl import CitationMixin, Maybe, MultiTask, llm_validator, openai_moderation
-from .function_calls import OpenAISchema, openai_function, openai_schema, Mode
+from .function_calls import OpenAISchema, openai_schema, Mode
 from .patch import apatch, patch
 
 __all__ = [
     "OpenAISchema",
-    "openai_function",
     "CitationMixin",
     "MultiTask",
     "Maybe",
