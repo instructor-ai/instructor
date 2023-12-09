1ddfce8d1b8669def5aa0ca7df8c83ebf64c1d44
Hongnan Gao <hongnangao@gmail.com>
2023-12-09 22:44:54 +0800

    continuous-integration: fix redundant env vars in .github/workflows/mypy.yml.

--- instructor/__init__.py
+++ instructor/__init__.py
@@ -1,21 +1,21 @@
-# from .distil import FinetuneFormat, Instructions
-# from .dsl import CitationMixin, Maybe, MultiTask, llm_validator, openai_moderation
-# from .function_calls import OpenAISchema, openai_function, openai_schema, Mode
-# from .patch import apatch, patch
+from .distil import FinetuneFormat, Instructions
+from .dsl import CitationMixin, Maybe, MultiTask, llm_validator, openai_moderation
+from .function_calls import OpenAISchema, openai_function, openai_schema, Mode
+from .patch import apatch, patch
 
-# __all__ = [
-#     "OpenAISchema",
-#     "openai_function",
-#     "CitationMixin",
-#     "MultiTask",
-#     "Maybe",
-#     "openai_schema",
-#     "Mode",
-#     "patch",
-#     "apatch",
-#     "llm_validator",
-#     "openai_moderation",
-#     "FinetuneFormat",
-#     "Instructions",
-#     "unpatch",
-# ]
+__all__ = [
+    "OpenAISchema",
+    "openai_function",
+    "CitationMixin",
+    "MultiTask",
+    "Maybe",
+    "openai_schema",
+    "Mode",
+    "patch",
+    "apatch",
+    "llm_validator",
+    "openai_moderation",
+    "FinetuneFormat",
+    "Instructions",
+    "unpatch",
+]
