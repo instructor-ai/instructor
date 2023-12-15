f96b74a50a9c377126927a38b79290324a1e58d4
Jason Liu <jason@jxnl.co>
2023-12-15 11:25:28 -0500

    format

--- tests/openai/util.py
+++ tests/openai/util.py
@@ -1,7 +1,6 @@
-import os
 import instructor
 
-models = ["gpt-3.5-turbo-1106", "gpt-4", "gpt-4-1106-preview"]
+models = ["gpt-3.5-turbo-1106", "gpt-4-1106-preview"]
 modes = [
     instructor.Mode.FUNCTIONS,
     instructor.Mode.JSON,
