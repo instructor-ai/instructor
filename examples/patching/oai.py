f5046b73fb561aa88071a8189a9431b57ca486ca
Jason Liu <jason@jxnl.co>
2024-01-27 12:05:33 -0500

    oai

---
+++ examples/patching/oai.py
@@ -0,0 +1,34 @@
+import os
+import instructor
+
+from openai import OpenAI
+from pydantic import BaseModel
+
+
+# By default, the patch function will patch the ChatCompletion.create and ChatCompletion.acreate methods. to support response_model parameter
+client = instructor.patch(
+    OpenAI(),
+    mode=instructor.Mode.TOOLS,
+)
+
+
+# Now, we can use the response_model parameter using only a base model
+# rather than having to use the OpenAISchema class
+class UserExtract(BaseModel):
+    name: str
+    age: int
+
+
+user: UserExtract = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    response_model=UserExtract,
+    messages=[
+        {"role": "user", "content": "Extract jason is 25 years old"},
+    ],
+)  # type: ignore
+
+print(user)
+{
+    "name": "Jason",
+    "age": 25,
+}
