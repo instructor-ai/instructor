803c2c6b217e91401061b931edce8d6e006c2310
Jason Liu <jason@jxnl.co>
2024-02-05 14:54:46 -0500

    bump

--- tests/test_patch.py
+++ tests/test_patch.py
@@ -1,16 +1,9 @@
 import functools
 
-import pytest
 from openai import AsyncOpenAI, OpenAI
-from openai.types.chat import ChatCompletionMessage, ChatCompletionMessageParam
-from openai.types.chat.chat_completion_message import FunctionCall
-from openai.types.chat.chat_completion_message_tool_call import (
-    ChatCompletionMessageToolCall,
-    Function,
-)
 
 import instructor
-from instructor.patch import OVERRIDE_DOCS, dump_message, is_async
+from instructor.patch import OVERRIDE_DOCS, is_async
 
 
 def test_patch_completes_successfully():
@@ -50,12 +43,3 @@ def test_override_docs():
     assert (
         "response_model" in OVERRIDE_DOCS
     ), "response_model should be in OVERRIDE_DOCS"
-
-
-def test_dump_message(
-    name_of_test: str,
-    message: ChatCompletionMessage,
-    expected: ChatCompletionMessageParam,
-):
-    #! Something is going on right now, but I don't have time to figure it out @jxnlco
-    assert dump_message(message) == expected, name_of_test
