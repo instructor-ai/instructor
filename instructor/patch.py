3239f405900c144018e675af67b01b1ada62deb2
Jason Liu <jason@jxnl.co>
2024-02-07 09:42:01 -0500

    remove pd

--- instructor/patch.py
+++ instructor/patch.py
@@ -3,7 +3,6 @@ import json
 import logging
 from collections.abc import Iterable
 from functools import wraps
-import pandas as pd
 from tenacity import Retrying, AsyncRetrying, stop_after_attempt, RetryError
 from json import JSONDecodeError
 from typing import (
