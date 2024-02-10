60c750ba6fa567cd88921d11ce14e1d08b014fec
Francis Beeson <leobeeson@gmail.com>
2024-02-10 22:21:06 +0000

    Include new model names in _alias.py
    ## Include New Model Names
    
    CLI does not include new OpenAI models. 
    
    ## Changes
    
    Added the following model names:
    
    * `gpt-3.5-turbo-0125`
    * `gpt-3.5-turbo-instruct`
    * `gpt-4-0125-preview`
    * `gpt-4-1106-preview`
    * `gpt-4-1106-vision-preview`
    
    ## Caveats
    
    The models `gpt-4-1106-vision-preview` and `gpt-3.5-turbo-instruct` might not be too relevant for users using `Instructor`, but I included them for completeness.

--- instructor/_types/_alias.py
+++ instructor/_types/_alias.py
@@ -3,8 +3,13 @@ from typing import Literal
 from typing_extensions import TypeAlias
 
 ModelNames: TypeAlias = Literal[
+    "gpt-3.5-turbo-0125",
+    "gpt-3.5-turbo-instruct",
     "gpt-3.5-turbo",
     "gpt-3.5-turbo-16k",
+    "gpt-4-0125-preview",
+    "gpt-4-1106-preview",
+    "gpt-4-1106-vision-preview",
     "gpt-4",
     "gpt-4-32k",
     "text-embedding-ada-002",
