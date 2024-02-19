db69264c8ba9f3060c789c51639d35daf0c502a4
Jason Liu <jason@jxnl.co>
2024-02-19 16:56:28 -0500

    add create option

--- instructor/patch.py
+++ instructor/patch.py
@@ -1,5 +1,7 @@
 import logging
+from pydoc import cli
 from typing import (
+    Callable,
     Union,
     overload,
 )
@@ -28,8 +30,17 @@ def patch(
     ...
 
 
+@overload
+def patch(
+    create: Callable,
+    mode: Mode = Mode.FUNCTIONS,
+):
+    ...
+
+
 def patch(
     client: Union[OpenAI, AsyncOpenAI] = None,
+    create: Callable = None,
     mode: Mode = Mode.FUNCTIONS,
 ) -> Union[InstructorOpenAI, InstructorAsyncOpenAI]:
     """
@@ -43,12 +54,16 @@ def patch(
     - `strict` parameter to use strict json parsing
     """
 
-    logger.debug(f"Patching `client.chat.completions.create` with {mode=}")
+    if client is not None:
+        logger.debug(f"Patching `client.chat.completions.create` with {mode=}")
 
-    if isinstance(client, AsyncOpenAI):
-        return InstructorAsyncOpenAI(client, mode=mode)
-    else:
-        return InstructorOpenAI(client, mode=mode)
+        if isinstance(client, AsyncOpenAI):
+            return InstructorAsyncOpenAI(client, mode=mode)
+        else:
+            return InstructorOpenAI(client, mode=mode)
+
+    if create is not None:
+        raise NotImplementedError("Patching a function is not yet supported in v1.0.0")
 
 
 def apatch(client: AsyncOpenAI, mode: Mode = Mode.FUNCTIONS):
