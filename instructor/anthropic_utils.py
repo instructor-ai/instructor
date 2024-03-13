bde35994ec418df7fe134c82fcf59d29ba7d8df1
shreya-51 <48033781+shreya-51@users.noreply.github.com>
2024-03-13 00:17:33 -0400

    add support for non-streaming List type

--- instructor/anthropic_utils.py
+++ instructor/anthropic_utils.py
@@ -1,5 +1,4 @@
 import re
-import json
 import xmltodict
 from pydantic import BaseModel
 import xml.etree.ElementTree as ET
@@ -11,6 +10,8 @@ T = TypeVar("T", bound=BaseModel)
 def json_to_xml(model: Type[BaseModel]) -> str:
     """Takes a Pydantic model and returns XML format for Anthropic function calling."""
     model_dict = model.model_json_schema()
+    
+    #TODO: model_dict structure is different for Partial classes during streaming, need to adjust for this for better prompting + List types
 
     root = ET.Element("tool_description")
     tool_name = ET.SubElement(root, "tool_name")
@@ -21,24 +22,35 @@ def json_to_xml(model: Type[BaseModel]) -> str:
     )
     parameters = ET.SubElement(root, "parameters")
     references = model_dict.get("$defs", {})
-    _add_params(parameters, model_dict, references)
-
-    return ET.tostring(root, encoding="unicode")
+    list_type_found = _add_params(parameters, model_dict, references)
+    
+    if list_type_found: # Need to append to system prompt for List type handling
+        return ET.tostring(root, encoding="unicode") + "\nFor any List[] types, include multiple <$PARAMETER_NAME>$PARAMETER_VALUE</$PARAMETER_NAME> tags for each item in the list."
+    else:
+        return ET.tostring(root, encoding="unicode")
 
 
 def _add_params(
     root: ET.Element, model_dict: Dict[str, Any], references: Dict[str, Any]
-) -> None:  # check to handle nested params with the same name?
+) -> bool: # Return value indiciates if we ever came across a param with type List
+    # TODO: check handling of nested params with the same name
     properties = model_dict.get("properties", {})
+    list_found = False
 
     for field_name, details in properties.items():
         parameter = ET.SubElement(root, "parameter")
         name = ET.SubElement(parameter, "name")
         name.text = field_name
         type_element = ET.SubElement(parameter, "type")
-        type_element.text = details.get(
-            "type", "unknown"
-        )  # Might be better to fail here if there is no type since pydantic models require types
+        
+        if details.get("type") == "array":
+            type_element.text = f"List[{details['title']}]"
+            list_found = True
+        else:
+            type_element.text = details.get(
+                "type", "unknown"
+            )  # Might be better to fail here if there is no type since pydantic models require types
+
         param_description = ET.SubElement(parameter, "description")
         param_description.text = f"The {field_name} of the {model_dict['title']} model"
 
@@ -46,12 +58,20 @@ def _add_params(
             isinstance(details, dict) and "$ref" in details
         ):  # Checking if there are nested params
             nested_params = ET.SubElement(parameter, "parameters")
-            _add_params(
+            list_found |= _add_params(
                 nested_params,
                 _resolve_reference(references, details["$ref"]),
                 references,
             )
-
+        elif details.get("type") == "array": # Handling for List[] type
+            nested_params = ET.SubElement(parameter, "parameters")
+            list_found |= _add_params(
+                nested_params,
+                _resolve_reference(references, details["items"]["$ref"]),
+                references,
+            )
+    
+    return list_found
 
 def _resolve_reference(references: Dict[str, Any], reference: str) -> Dict[str, Any]:
     parts = reference.split("/")[2:]  # Remove "#" and "$defs"
@@ -60,7 +80,7 @@ def _resolve_reference(references: Dict[str, Any], reference: str) -> Dict[str, 
     return references
 
 
-def extract_xml(content: str) -> str:
+def extract_xml(content: str) -> str: # Currently assumes 1 function call only
     """Extracts XML content in Anthropic's schema from a string."""
     pattern = r"<function_calls>.*?</function_calls>"
     matches = re.findall(pattern, content, re.DOTALL)
