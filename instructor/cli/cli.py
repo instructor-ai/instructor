79f690494fd1dad1d4b52efb8d041912aca1f102
Ivan Leo <ivanleomk@gmail.com>
2024-06-19 15:39:32 +0800

    feat: new cli for batch jobs (#754)
    Co-authored-by: Jason Liu <jxnl@users.noreply.github.com>
    
    Added a new CLI for batch jobs

--- instructor/cli/cli.py
+++ instructor/cli/cli.py
@@ -3,6 +3,7 @@ import instructor.cli.jobs as jobs
 import instructor.cli.files as files
 import instructor.cli.usage as usage
 import instructor.cli.hub as hub
+import instructor.cli.batch as batch
 
 app = typer.Typer()
 
@@ -10,6 +11,7 @@ app.add_typer(jobs.app, name="jobs", help="Monitor and create fine tuning jobs")
 app.add_typer(files.app, name="files", help="Manage files on OpenAI's servers")
 app.add_typer(usage.app, name="usage", help="Check OpenAI API usage data")
 app.add_typer(hub.app, name="hub", help="Interact with the instructor hub")
+app.add_typer(batch.app, name="batch", help="Manage OpenAI Batch jobs")
 
 
 @app.command()
