c02f32a77db79e825f2f896afa608ee8a9d321da
Ivan Leo <ivanleomk@gmail.com>
2024-07-14 09:48:44 +0800

    Removed braintrust proxy

--- tests/llm/test_openai/conftest.py
+++ tests/llm/test_openai/conftest.py
@@ -1,39 +1,13 @@
 # conftest.py
 from openai import AsyncOpenAI, OpenAI
 import pytest
-import os
-
-try:
-    import braintrust
-
-    wrap_openai = braintrust.wrap_openai
-except ImportError:
-
-    def wrap_openai(x):
-        return x
 
 
 @pytest.fixture(scope="session")
 def client():
-    if os.environ.get("BRAINTRUST_API_KEY"):
-        yield wrap_openai(
-            OpenAI(
-                api_key=os.environ["BRAINTRUST_API_KEY"],
-                base_url="https://braintrustproxy.com/v1",
-            )
-        )
-    else:
-        yield OpenAI()
+    yield OpenAI()
 
 
 @pytest.fixture(scope="session")
 def aclient():
-    if os.environ.get("BRAINTRUST_API_KEY"):
-        yield wrap_openai(
-            AsyncOpenAI(
-                api_key=os.environ["BRAINTRUST_API_KEY"],
-                base_url="https://braintrustproxy.com/v1",
-            )
-        )
-    else:
-        yield AsyncOpenAI()
+    yield AsyncOpenAI()
