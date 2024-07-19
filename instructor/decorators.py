84472c20f0ad861df65f8544ba0649922a2cab5d
Ivan Leo <ivanleomk@gmail.com>
2024-07-19 14:11:42 +0800

    Fixed up a new model decorator

--- instructor/decorators.py
+++ instructor/decorators.py
@@ -3,6 +3,7 @@ from inspect import signature
 from pydantic import ValidationInfo
 
 ASYNC_VALIDATOR_KEY = "__async_validator__"
+ASYNC_MODEL_VALIDATOR_KEY = "__async_model_validator__"
 T = TypeVar("T", bound=Callable[..., Any])
 
 
@@ -36,6 +37,29 @@ def async_field_validator(field: str, *fields: str) -> Callable[[T], T]:
 
         setattr(
             func, ASYNC_VALIDATOR_KEY, (field_names, func, requires_validation_context)
+        )
+        return func
+
+    return decorator
+
+
+def async_model_decorator() -> Callable[[T], T]:
+    def decorator(func: T) -> T:
+        params = signature(func).parameters
+        requires_validation_context = False
+        if len(params) == 3:
+            if not params["info"]:
+                raise ValueError(
+                    "Async validator can only have a value parameter and an optional info parameter"
+                )
+            if params["info"].annotation != ValidationInfo:
+                raise ValueError(
+                    "Async validator info parameter must be of type ValidationInfo"
+                )
+            requires_validation_context = True
+
+        setattr(
+            func, ASYNC_MODEL_VALIDATOR_KEY, ("*", func, requires_validation_context)
         )
         return func
 
