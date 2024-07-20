8508cbcd50728e2adf9278f63cfb29dee53085ae
Ivan Leo <ivanleomk@gmail.com>
2024-07-21 01:37:57 +0800

    Fixed up parallel

--- tests/llm/test_openai/test_parallel.py
+++ tests/llm/test_openai/test_parallel.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 from typing import Literal, Union
 from collections.abc import Iterable
 from pydantic import BaseModel
+from itertools import product
 import pytest
 import instructor
+from .util import models, modes
 
 
 class Weather(BaseModel):
@@ -50,10 +52,11 @@ def test_sync_parallel_tools_or(client):
 
 
 @pytest.mark.asyncio
-async def test_async_parallel_tools_or(aclient):
-    client = instructor.from_openai(aclient, mode=instructor.Mode.PARALLEL_TOOLS)
+@pytest.mark.parametrize("model, mode", product(models, modes))
+async def test_async_parallel_tools_or(model, mode, aclient):
+    client = instructor.from_openai(aclient, mode=mode)
     resp = await client.chat.completions.create(
-        model="gpt-4-turbo-preview",
+        model=model,
         messages=[
             {"role": "system", "content": "You must always use tools"},
             {
