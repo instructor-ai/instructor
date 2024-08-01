048f7390005f69de2e5382357e723e1f2edf3f08
Ivan Leo <ivanleomk@gmail.com>
2024-08-01 19:59:29 +0800

    Reverted removal of type ignore

--- instructor/dsl/citation.py
+++ instructor/dsl/citation.py
@@ -57,7 +57,7 @@ class CitationMixin(BaseModel):
         description="List of unique and specific substrings of the quote that was used to answer the question.",
     )
 
-    @model_validator(mode="after")
+    @model_validator(mode="after")  # type: ignore[misc]
     def validate_sources(self, info: ValidationInfo) -> "CitationMixin":
         """
         For each substring_phrase, find the span of the substring_phrase in the context.
