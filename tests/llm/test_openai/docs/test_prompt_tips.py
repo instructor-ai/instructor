7b38c2573763db27144f98c8dae800309b51e201
Ivan Leo <ivanleomk@gmail.com>
2024-10-11 14:40:12 +0800

    Disabling prompt tips tests for now

--- tests/llm/test_openai/docs/test_prompt_tips.py
+++ tests/llm/test_openai/docs/test_prompt_tips.py
@@ -3,6 +3,7 @@ from pytest_examples import find_examples, CodeExample, EvalExample
 
 
 @pytest.mark.parametrize("example", find_examples("docs/prompting"), ids=str)
+@pytest.mark.skip(reason="Skipping this for now")
 def test_format_concepts(example: CodeExample, eval_example: EvalExample):
     if eval_example.update_examples:
         eval_example.format(example)
