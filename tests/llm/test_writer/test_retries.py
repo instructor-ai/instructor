d888a2788a7efa3b8b125087357e5bc2f3f4be94
Johannes <tolstoy.work@gmail.com>
2024-11-15 10:44:32 +0100

    test: add tests for async retries

--- tests/llm/test_writer/test_retries.py
+++ tests/llm/test_writer/test_retries.py
@@ -3,7 +3,7 @@ from pydantic import AfterValidator, BaseModel, Field
 import pytest
 import instructor
 from itertools import product
-from writerai import Writer
+from writerai import AsyncWriter, Writer
 from .util import models, modes
 
 
@@ -21,7 +21,7 @@ class UserDetail(BaseModel):
 
 
 @pytest.mark.parametrize("model, mode", product(models, modes))
-def test_upper_case(model: str, mode: instructor.Mode):
+def test_writer_upper_case(model: str, mode: instructor.Mode):
     client = instructor.from_writer(client=Writer(), mode=mode)
     response = client.chat.completions.create(
         model=model,
@@ -35,7 +35,7 @@ def test_upper_case(model: str, mode: instructor.Mode):
 
 
 @pytest.mark.parametrize("model, mode", product(models, modes))
-def test_upper_case_tenacity(model: str, mode: instructor.Mode):
+def test_writer_upper_case_tenacity(model: str, mode: instructor.Mode):
     client = instructor.from_writer(client=Writer(), mode=mode)
     from tenacity import Retrying, stop_after_attempt, wait_fixed
 
@@ -45,6 +45,44 @@ def test_upper_case_tenacity(model: str, mode: instructor.Mode):
     )
 
     response = client.chat.completions.create(
+        model=model,
+        response_model=UserDetail,
+        messages=[
+            {"role": "user", "content": "Extract `jason is 12`"},
+        ],
+        max_retries=retries,
+    )
+    assert response.first_name == "JASON"
+
+
+@pytest.mark.parametrize("model, mode", product(models, modes))
+@pytest.mark.asyncio()
+async def test_writer_upper_case_async(model: str, mode: instructor.Mode):
+    client = instructor.from_writer(client=AsyncWriter(), mode=mode)
+
+    response = await client.chat.completions.create(
+        model=model,
+        response_model=UserDetail,
+        messages=[
+            {"role": "user", "content": "Extract `jason is 12`"},
+        ],
+        max_retries=3,
+    )
+    assert response.first_name == "JASON"
+
+
+@pytest.mark.parametrize("model, mode", product(models, modes))
+@pytest.mark.asyncio()
+async def test_writer_upper_case_tenacity_async(model: str, mode: instructor.Mode):
+    client = instructor.from_writer(client=AsyncWriter(), mode=mode)
+    from tenacity import AsyncRetrying, stop_after_attempt, wait_fixed
+
+    retries = AsyncRetrying(
+        stop=stop_after_attempt(2),
+        wait=wait_fixed(1),
+    )
+
+    response = await client.chat.completions.create(
         model=model,
         response_model=UserDetail,
         messages=[
