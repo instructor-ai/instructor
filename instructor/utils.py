320e652d0715dfbfc91fb8c90c0a2210b3631b11
Ivan Leo <ivanleomk@gmail.com>
2024-11-17 20:56:58 +0800

    fix: fixed up partials so that it accounts for writer returning the entire item at the end

--- instructor/utils.py
+++ instructor/utils.py
@@ -14,7 +14,7 @@ from typing import (
     Union,
     TypedDict,
     TypeVar,
-    cast
+    cast,
 )
 from pydantic import BaseModel
 import os
@@ -134,26 +134,6 @@ async def extract_json_from_stream_async(
                 yield char
 
 
-def extract_json_from_stream_writer(chunks: Iterable[str]) -> Generator[str, None, None]:
-    summary_chunk_regexp = r"\{.*\}"
-    for chunk in chunks:
-        if not re.search(summary_chunk_regexp, chunk):
-            yield chunk
-        else:
-            yield "}"
-
-
-async def extract_json_from_stream_writer_async(
-        chunks: AsyncGenerator[str, None],
-) -> AsyncGenerator[str, None]:
-    summary_chunk_regexp = r"\{.*\}"
-    async for chunk in chunks:
-        if not re.search(summary_chunk_regexp, chunk):
-            yield chunk
-        else:
-            yield "}"
-
-
 def update_total_usage(
     response: T_Model | None,
     total_usage: OpenAIUsage | AnthropicUsage,
@@ -166,10 +146,16 @@ def update_total_usage(
         total_usage.completion_tokens += response_usage.completion_tokens or 0
         total_usage.prompt_tokens += response_usage.prompt_tokens or 0
         total_usage.total_tokens += response_usage.total_tokens or 0
-        if (rtd := response_usage.completion_tokens_details) and (ttd := total_usage.completion_tokens_details):
+        if (rtd := response_usage.completion_tokens_details) and (
+            ttd := total_usage.completion_tokens_details
+        ):
             ttd.audio_tokens = (ttd.audio_tokens or 0) + (rtd.audio_tokens or 0)
-            ttd.reasoning_tokens = (ttd.reasoning_tokens or 0) + (rtd.reasoning_tokens or 0)
-        if (rpd := response_usage.prompt_tokens_details) and (tpd := total_usage.prompt_tokens_details):
+            ttd.reasoning_tokens = (ttd.reasoning_tokens or 0) + (
+                rtd.reasoning_tokens or 0
+            )
+        if (rpd := response_usage.prompt_tokens_details) and (
+            tpd := total_usage.prompt_tokens_details
+        ):
             tpd.audio_tokens = (tpd.audio_tokens or 0) + (rpd.audio_tokens or 0)
             tpd.cached_tokens = (tpd.cached_tokens or 0) + (rpd.cached_tokens or 0)
         response.usage = total_usage  # Replace each response usage with the total usage
