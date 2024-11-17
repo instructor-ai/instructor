bb0143cb1ec1909c0cd1530c3f430d1a2daac015
Ivan Leo <ivanleomk@gmail.com>
2024-11-17 21:34:49 +0800

    fix: remove unused import

--- instructor/utils.py
+++ instructor/utils.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import inspect
 import json
 import logging
-import re
 from collections.abc import AsyncGenerator, Generator, Iterable
 from typing import (
     TYPE_CHECKING,
