9df4b8c3cd449055be7847fe6702089cb9d3283c
Chris Ruppelt <chris.ruppelt1@gmail.com>
2024-12-06 04:04:21 -0500

    idk

--- instructor/utils.py
+++ instructor/utils.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import inspect
 import json
 import logging
+import os
 from collections.abc import AsyncGenerator, Generator, Iterable
 from typing import (
     TYPE_CHECKING,
