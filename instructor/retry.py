147e0adcdf04ad2e9221c3fe1d22c79a5e80581d
hnalla <harsha.nalla@nlplogix.com>
2024-12-12 14:43:30 -0500

    fixes #1252

--- instructor/retry.py
+++ instructor/retry.py
@@ -73,7 +73,7 @@ def initialize_usage(mode: Mode) -> CompletionUsage | Any:
     """
     total_usage = CompletionUsage(completion_tokens=0, prompt_tokens=0, total_tokens=0,
         completion_tokens_details = CompletionTokensDetails(audio_tokens=0, reasoning_tokens=0),
-        prompt_token_details = PromptTokensDetails(audio_tokens=0, cached_tokens=0)
+        prompt_tokens_details = PromptTokensDetails(audio_tokens=0, cached_tokens=0)
     )
     if mode in {Mode.ANTHROPIC_TOOLS, Mode.ANTHROPIC_JSON}:
         from anthropic.types import Usage as AnthropicUsage
