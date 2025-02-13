44d215c405afa6291c0b346423f42ad3ca897c1c
GeeJay <33249440+GeeJay12@users.noreply.github.com>
2025-02-14 03:08:49 +0530

    use gemini client in llm_validator

---
+++ tests/llm/test_gemini/test_validators.py
@@ -0,0 +1,38 @@
+from itertools import product
+import pytest
+
+import instructor
+
+from typing import Annotated
+from pydantic import BaseModel, BeforeValidator, ValidationError
+
+from instructor.dsl.validators import llm_validator
+import google.generativeai as genai
+from .util import models, modes
+
+
+@pytest.mark.parametrize("model, mode", product(models, modes))
+def test_runmodel_validator_error(model, mode, client):
+    client = instructor.from_gemini(
+        genai.GenerativeModel(
+            model,
+        ),
+        mode=mode,
+    )
+
+    class QuestionAnswerNoEvil(BaseModel):
+        question: str
+        answer: Annotated[
+            str,
+            BeforeValidator(
+                llm_validator(
+                    "don't say objectionable things", model=model, client=client
+                )
+            ),
+        ]
+
+    with pytest.raises(ValidationError):
+        QuestionAnswerNoEvil(
+            question="What is the meaning of life?",
+            answer="The meaning of life is to be evil and steal",
+        )
