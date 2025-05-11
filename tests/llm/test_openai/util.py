64c02fa7202d05b531c2179666b226074f0a0e2e
Ivan Leo <ivanleomk@gmail.com>
2025-05-11 15:07:03 +0800

    fix: add responses support

--- tests/llm/test_openai/util.py
+++ tests/llm/test_openai/util.py
@@ -3,7 +3,7 @@ import instructor
 models = ["gpt-4o-mini"]
 modes = [
     instructor.Mode.TOOLS,
-    # instructor.Mode.TOOLS_STRICT,
+    instructor.Mode.TOOLS_STRICT,
     instructor.Mode.RESPONSES_TOOLS,
     instructor.Mode.RESPONSES_TOOLS_WITH_INBUILT_TOOLS,
 ]
